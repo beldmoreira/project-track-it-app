7f36643604962063e0231db115b46ca58ca53bc0
Isabela <isabela_cmoreira@hotmail.com>
2022-04-04 00:51:44 -0300

    Footer component was created with its matching style

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import {FooterStyle} from "./style";
+
+
+export default function Footer(){
+    return(
+        <FooterStyle>
+
+        </FooterStyle>
+
+    );
+}
---
+++ src/components/Footer/style.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const FooterStyle = styled.footer`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: fixed;
+  left: 0px;
+  bottom: 0px;
+  background: #FFFFFF;
+`;
+export{
+    FooterStyle
+}
